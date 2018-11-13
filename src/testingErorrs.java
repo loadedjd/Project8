75867c82272e8cb780e9e5e3e1fb131fc529b574
Jared Williams <williamsjared62@gmail.com>
2018-11-13 12:47:16 -0500

    Rocky's Stuff

---
+++ src/testingErorrs.java
@@ -0,0 +1,44 @@
+import components.program.Program;
+import components.program.Program1;
+import components.queue.Queue;
+import components.simplereader.SimpleReader;
+import components.simplereader.SimpleReader1L;
+import components.simplewriter.SimpleWriter;
+import components.simplewriter.SimpleWriter1L;
+import components.utilities.Tokenizer;
+
+public class testingErorrs {
+
+    /**
+     * Main method.
+     *
+     * @param args
+     *            the command line arguments
+     */
+    public static void main(String[] args) {
+        SimpleReader in = new SimpleReader1L();
+        SimpleWriter out = new SimpleWriter1L();
+        /*
+         * Get input file name
+         */
+        out.print("Enter valid BL program file name: ");
+        String fileName = "test/program1.bl";
+        /*
+         * Parse input file
+         */
+        out.println("*** Parsing input file ***");
+        Program p = new Program1();
+        SimpleReader file = new SimpleReader1L(fileName);
+        Queue<String> tokens = Tokenizer.tokens(file);
+        file.close();
+        p.parse(tokens);
+        /*
+         * Pretty print the program
+         */
+        out.println("*** Pretty print of parsed program ***");
+        p.prettyPrint(out);
+
+        in.close();
+        out.close();
+    }
+}
